ce43c8f6253ed3bfd0be35d8bdfd03dd63342538
Gabe Fidder <gabfid@gmail.com>
2023-10-16 23:51:27 -0500

    fix: fixing build

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -2,16 +2,13 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeExternalLinks from "rehype-external-links";
 import rehypeSlug from "rehype-slug";
 import remarkCodeTitles from "remark-code-titles";
 
-/**
- * @link https://astro.build/config
- */
+/** @link https://astro.build/config */
 export default defineConfig({
   site: `https://create.t3.gg`,
   markdown: {
@@ -48,7 +45,6 @@ export default defineConfig({
   },
   integrations: [
     react(),
-    tailwind({ config: { applyBaseStyles: false } }),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
